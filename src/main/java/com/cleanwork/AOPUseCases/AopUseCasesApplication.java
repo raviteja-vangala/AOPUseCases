2d67f4bc410d4c7ae797ebb7ddfb2416be8db060
vangala.raviteja <vangala.raviteja@mpokket.com>
2024-10-27 14:55:00 +0530

    Initial Project Setup

---
+++ src/main/java/com/cleanwork/AOPUseCases/AopUseCasesApplication.java
@@ -0,0 +1,13 @@
+package com.cleanwork.AOPUseCases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopUseCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopUseCasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleanwork/AOPUseCases/AopUseCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanwork.AOPUseCases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopUseCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
